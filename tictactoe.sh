b11c6989dd716b6c78629f5a4e84ce2c6009f959
Revati Tekale <revitekale1910@gmail.com>
2020-03-03 12:46:22 +0530

    ADDED:Display board to choose position

--- tictactoe.sh
+++ tictactoe.sh
@@ -1,31 +1,18 @@
 #!/bin/bash -x
 echo "Welcome to Tictactoe game"
-row=3
-column=3
+ROW=3
+COLUMN=3
+
 function initializeBoard(){  
-	for((r=0; r<$row; r++))
+	for((r=0; r<$ROW; r++))
 	do
-		for((	c=0; c<$column; c++))
+		for((	c=0; c<$COLUMN; c++))
 		do
 			board[$r,$c]="-"
 		done
 	done
 }
 initializeBoard
-function displayBoard(){  
-	for((r=0;r<$row;r++))
-	do
-		echo "----------------"
-		for(( c=0; c<$row; c++))
-		do
-			echo -n "|" ${board[$r,$c]} " " 
-		done
-		echo 
-	done
-	echo "----------------"
-
-}
-displayBoard
 assignLetter()   
 {
    echo "Your latter X"
@@ -42,4 +29,17 @@ whoPlayFirst()
    fi
 }
 whoPlayFirst
+function displayBoard(){  
+   for((r=0;r<$ROW;r++))
+   do
+      echo "----------------"
+      for(( c=0; c<$COLUMN; c++))
+      do
+         echo -n "|" ${board[$r,$c]} " " 
+      done
+      echo 
+   done
+   echo "----------------"
 
+}
+displayBoard
