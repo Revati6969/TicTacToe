1e47341b3283e7d6410988b4ecccb154edb15047
Revati Tekale <revitekale1910@gmail.com>
2020-03-03 09:50:45 +0530

    ADDED:Display board

--- tictactoe.sh
+++ tictactoe.sh
@@ -1,4 +1,28 @@
 #!/bin/bash -x
+echo "Welcome to Tictactoe game"
+row=3
+column=3
+function initializeBoard(){  #Uc1 function to create board
+	for((r=0; r<$row; r++))
+	do
+		for((	c=0; c<$column; c++))
+		do
+			board[$r,$c]="-"
+		done
+	done
+}
+initializeBoard
+function displayBoard(){  #finction to displayboard
+	for((r=0;r<$row;r++))
+	do
+		echo "----------------"
+		for(( c=0; c<$row; c++))
+		do
+			echo -n "|" ${board[$r,$c]} " " 
+		done
+		echo 
+	done
+	echo "----------------"
 
-echo "Welcome to tic tac toe game"
-
+}
+displayBoard
